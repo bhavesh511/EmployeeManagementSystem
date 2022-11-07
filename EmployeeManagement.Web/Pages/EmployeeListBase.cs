3477b0310a1a7e05fd078cb247a30aa5c68c22b8
Bhavesh <bhavesh511@outlook.com>
2022-11-07 17:32:28 +0530

    Add project files.

---
+++ EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections;
+using EmployeeManagement.Models;
+using System.Collections.Generic;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class EmployeeListBase : ComponentBase
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+    }
+}
